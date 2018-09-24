97d71bb287abce8ab3226518b5fa5e3969a4df25
montezuma93 <hmr@riki5.de>
2018-09-24 18:44:51 +0200

    Changed tests

--- ObjectTest.py
+++ ObjectTest.py
@@ -1,9 +1,9 @@
 import unittest
 from Object import ConcreteObject
 
-class TestRelation(unittest.TestCase):
+class TestObject(unittest.TestCase):
 
     def test_relation_category_is_initialized_correctly(self):
-        concrete_object = ConcreteObject("cardinal_direction", "north")
-        self.assertEqual(concrete_object.category_name, 'cardinal_direction')
-        self.assertEqual(concrete_object.name, 'north')
+        concrete_object = ConcreteObject("city", "Berlin")
+        self.assertEqual("city", concrete_object.category_name)
+        self.assertEqual("Berlin", concrete_object.name)
