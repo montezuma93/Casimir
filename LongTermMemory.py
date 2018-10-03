525e78a784316fc5805f4a9cc906300dbef0cec3
montezuma93 <hmr@riki5.de>
2018-10-03 16:26:43 +0200

    Added logic to add initial activation and tests

--- LongTermMemory.py
+++ LongTermMemory.py
@@ -2,6 +2,14 @@ from collections import OrderedDict
 
 class LongTermMemory:
 
+    DECAY =  0.9
+    RELATION_OBJECT_LINK_WEIGHT = 0.75
+    OBJECT_RELATION_LINK_WEIGHT = 0.25
+    FIRING_THRESHOLD = 0.5
+    INITIAL_ACTIVATION_ON = 1
+    INITIAL_ACTIVATION_OFF = 0
+
+
     def __init__(self):
         self.stored_relations = OrderedDict()
         self.stored_objects = OrderedDict()
@@ -26,11 +34,38 @@ class LongTermMemory:
             else:
                 self.stored_objects[concrete_object.name] = [object_to_store]
 
-    def receive_knowledge_fragments(self, relation_category, object1, object2):
-        attention_rate = 1
-        threshold = 0.5
-        while(attention_rate > threshold):
-            self.stored_relations[relation_category.category_name]
+    def receive_knowledge_fragments(self, relation_type, object1, object2):
+        self._spread_activation(relation_type, object1, object2)
+        self._get_most_activated_fragments()
+        
+    
+    def _spread_activation(self, intial_relation_type, initial_object1, initial_object2):
+        self._set_initial_activation(intial_relation_type, initial_object1, initial_object2)
+        while(self._firing_node_exists()):
+            return "true"
+
+    def _set_initial_activation(self, intial_relation_type, initial_object1, initial_object2):
+        for relation_type, relation_to_objects_mappings in self.stored_relations.items():
+            for relation_to_objects_mapping in relation_to_objects_mappings:
+                if (relation_type == intial_relation_type):
+                    relation_to_objects_mapping.activation = self.INITIAL_ACTIVATION_ON
+                else:
+                    relation_to_objects_mapping.activation = self.INITIAL_ACTIVATION_OFF
+        for object_name, object_to_relation_mappings in self.stored_objects.items():
+            for object_to_relation_mapping in object_to_relation_mappings:
+                if(object_name == initial_object1.name or object_name == initial_object2.name):
+                    object_to_relation_mapping.activation = self.INITIAL_ACTIVATION_ON
+                else:
+                    object_to_relation_mapping.activation = self.INITIAL_ACTIVATION_OFF
+    
+    def _firing_node_exists(self):
+        return "true"
+    
+    def _get_most_activated_fragments(self):
+        return "true"
+
+
+
 
 class RelationToObjectsMapping:
     def __init__(self, relation, object_list):
--- test_LongTermMemory.py
+++ test_LongTermMemory.py
@@ -10,9 +10,9 @@ class TestLongTermMemory(unittest.TestCase):
         paris_city_object = CityObject("Paris")
         london_city_object = CityObject("London")
         north_cardinal_relation = NorthCardinalRelation()
-        relation_to_object_mapping_1 = RelationToObjectsMapping(north_cardinal_relation, [london_city_object, paris_city_object])
+        relation_to_objects_mapping_1 = RelationToObjectsMapping(north_cardinal_relation, [london_city_object, paris_city_object])
 
-        long_term_memory.save_relation_object_mapping(relation_to_object_mapping_1)
+        long_term_memory.save_relation_object_mapping(relation_to_objects_mapping_1)
 
         self.assertEqual(long_term_memory.stored_relations[RelationType.CardinalRelation][0].relation.relation_type, RelationType.CardinalRelation)
         self.assertEqual(long_term_memory.stored_relations[RelationType.CardinalRelation][0].relation.name, CardinalRelationName.North)
@@ -24,13 +24,13 @@ class TestLongTermMemory(unittest.TestCase):
         paris_city_object = CityObject("Paris")
         london_city_object = CityObject("London")
         north_cardinal_relation = NorthCardinalRelation()
-        relation_to_object_mapping_1 = RelationToObjectsMapping(north_cardinal_relation, [london_city_object, paris_city_object])
+        relation_to_objects_mapping_1 = RelationToObjectsMapping(north_cardinal_relation, [london_city_object, paris_city_object])
         kairo_city_object = CityObject("Kairo")
         south_cardinal_relation = SouthCardinalRelation()
-        relation_to_object_mapping_2 = RelationToObjectsMapping(south_cardinal_relation, [kairo_city_object, paris_city_object])
+        relation_to_objects_mapping_2 = RelationToObjectsMapping(south_cardinal_relation, [kairo_city_object, paris_city_object])
 
-        long_term_memory.save_relation_object_mapping(relation_to_object_mapping_1)
-        long_term_memory.save_relation_object_mapping(relation_to_object_mapping_2)
+        long_term_memory.save_relation_object_mapping(relation_to_objects_mapping_1)
+        long_term_memory.save_relation_object_mapping(relation_to_objects_mapping_2)
 
         self.assertEqual(long_term_memory.stored_relations[RelationType.CardinalRelation][0].relation.name, CardinalRelationName.North)
         self.assertEqual(long_term_memory.stored_relations[RelationType.CardinalRelation][0].object_list[0].name, "London")
@@ -44,20 +44,49 @@ class TestLongTermMemory(unittest.TestCase):
         paris_city_object = CityObject("Paris")
         london_city_object = CityObject("London")
         north_cardinal_relation = NorthCardinalRelation()
-        relation_to_object_mapping_1 = RelationToObjectsMapping(north_cardinal_relation, [london_city_object, paris_city_object])
+        relation_to_objects_mapping_1 = RelationToObjectsMapping(north_cardinal_relation, [london_city_object, paris_city_object])
 
         france_country_object = CountryObject("France")
         part_of_topological_relation = PartOfTopologicalRelation()
-        relation_to_object_mapping_2 = RelationToObjectsMapping(part_of_topological_relation, [paris_city_object, france_country_object])
+        relation_to_objects_mapping_2 = RelationToObjectsMapping(part_of_topological_relation, [paris_city_object, france_country_object])
 
-        long_term_memory.save_relation_object_mapping(relation_to_object_mapping_1)
-        long_term_memory.save_relation_object_mapping(relation_to_object_mapping_2)
+        long_term_memory.save_relation_object_mapping(relation_to_objects_mapping_1)
+        long_term_memory.save_relation_object_mapping(relation_to_objects_mapping_2)
 
-        self.assertEqual(long_term_memory.stored_relations[RelationType.CardinalRelation][0], relation_to_object_mapping_1)
-        self.assertEqual(long_term_memory.stored_relations[RelationType.TopologicalRelation][0], relation_to_object_mapping_2)
+        self.assertEqual(long_term_memory.stored_relations[RelationType.CardinalRelation][0], relation_to_objects_mapping_1)
+        self.assertEqual(long_term_memory.stored_relations[RelationType.TopologicalRelation][0], relation_to_objects_mapping_2)
         self.assertEqual(long_term_memory.stored_objects["Paris"][0].relation_type, RelationType.CardinalRelation)
         self.assertEqual(long_term_memory.stored_objects["Paris"][0].reference_number, 0)
         self.assertEqual(long_term_memory.stored_objects["Paris"][0].stored_object, paris_city_object)
         self.assertEqual(long_term_memory.stored_objects["Paris"][1].relation_type, RelationType.TopologicalRelation)
         self.assertEqual(long_term_memory.stored_objects["Paris"][1].reference_number, 0)
         self.assertEqual(long_term_memory.stored_objects["Paris"][1].stored_object, paris_city_object)
+        self.assertEqual(long_term_memory.stored_objects["France"][0].stored_object, france_country_object)
+
+    def test_initial_activation_works_correctly(self):
+        long_term_memory = LongTermMemory()
+
+        paris_city_object = CityObject("Paris")
+        london_city_object = CityObject("London")
+        north_cardinal_relation = NorthCardinalRelation()
+        relation_to_objects_mapping_1 = RelationToObjectsMapping(north_cardinal_relation, [london_city_object, paris_city_object])
+
+        kairo_city_object = CityObject("Kairo")
+        south_cardinal_relation = SouthCardinalRelation()
+        relation_to_objects_mapping_2 = RelationToObjectsMapping(south_cardinal_relation, [kairo_city_object, paris_city_object])
+
+        france_country_object = CountryObject("France")
+        part_of_topological_relation = PartOfTopologicalRelation()
+        relation_to_objects_mapping_3 = RelationToObjectsMapping(part_of_topological_relation, [paris_city_object, france_country_object])
+
+        aberdeen_city_object = CityObject("Aberdeen")
+        relation_to_objects_mapping_3 = RelationToObjectsMapping(north_cardinal_relation, [aberdeen_city_object, london_city_object])
+
+        long_term_memory.save_relation_object_mapping(relation_to_objects_mapping_1)
+        long_term_memory.save_relation_object_mapping(relation_to_objects_mapping_2)
+        long_term_memory.save_relation_object_mapping(relation_to_objects_mapping_3)
+
+        long_term_memory._set_initial_activation(north_cardinal_relation.relation_type, aberdeen_city_object, london_city_object)
+        self.assertEqual(long_term_memory.stored_relations[RelationType.CardinalRelation][0].activation, 1)
+        self.assertEqual(long_term_memory.stored_objects["Paris"][0].activation, 0)
+        self.assertEqual(long_term_memory.stored_objects["London"][0].activation, 1)
